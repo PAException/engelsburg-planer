f3005badfd34277d119f5269c81a16f237fc6488
Paul Huerkamp <35733278+PAException@users.noreply.github.com>
2023-12-01 11:40:08 +0100

    Added automatic build script

---
+++ build.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+#######################################
+# Flutter Version Increment and Build Script
+#######################################
+
+# Script Usage:
+#   - This script increments the version number in the Flutter project's pubspec.yaml file.
+#   - It provides options to increment the MAJOR, MINOR, or PATCH version components.
+#   - Release builds for iOS and Android can be disabled using the --no-build option.
+
+# Usage:
+#   ./build.sh [OPTIONS]
+
+# Available Options:
+#   --major          Increment the MAJOR version component.
+#   --minor          Increment the MINOR version component.
+#   --patch          Increment the PATCH version component.
+#   --no-build       Disable release builds for iOS and Android.
+
+# Function to increment version components in pubspec.yaml
+increment_flutter_version() {
+    PUBSPEC_FILE="pubspec.yaml"
+    CURRENT_VERSION=$(awk '/version:/ {print $2}' "$PUBSPEC_FILE" | tr -d "'")
+    MAJOR=$(echo "$CURRENT_VERSION" | cut -d. -f1)
+    MINOR=$(echo "$CURRENT_VERSION" | cut -d. -f2)
+    PATCH=$(echo "$CURRENT_VERSION" | cut -d. -f3 | cut -d+ -f1)
+    BUILD=$(echo "$CURRENT_VERSION" | cut -d+ -f2)
+
+    if [ "$1" == "--major" ]; then
+        NEW_VERSION="$((MAJOR + 1)).0.0+0"
+    elif [ "$1" == "--minor" ]; then
+        NEW_VERSION="$MAJOR.$((MINOR + 1)).0+0"
+    elif [ "$1" == "--patch" ]; then
+        NEW_VERSION="$MAJOR.$MINOR.$((PATCH + 1))+0"
+    else
+        NEW_VERSION="$MAJOR.$MINOR.$PATCH+$((BUILD + 1))"
+    fi
+
+    awk -v current_version="$CURRENT_VERSION" -v new_version="$NEW_VERSION" \
+    '$1 == "version:" && $2 == current_version { $2 = new_version } { print }' \
+    "$PUBSPEC_FILE" > temp && mv temp "$PUBSPEC_FILE"
+
+    echo "Flutter version incremented to $NEW_VERSION"
+}
+
+# Function to execute Flutter release builds for iOS and Android
+execute_flutter_release_builds() {
+    flutter build ipa --release
+    flutter build appbundle --release
+}
+
+# Increment version regardless of --no-build presence
+if [ "$1" == "--major" ] || [ "$1" == "--minor" ] || [ "$1" == "--patch" ]; then
+    increment_flutter_version "$1"
+elif [ "$2" == "--major" ] || [ "$2" == "--minor" ] || [ "$2" == "--patch" ]; then
+    increment_flutter_version "$2"
+else
+    increment_flutter_version
+fi
+
+# Check for --no-build option to disable release builds
+if [[ "$1" == "--no-build" || "$2" == "--no-build" ]]; then
+    echo "Release builds disabled."
+else
+    execute_flutter_release_builds
+fi
